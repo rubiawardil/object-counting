b5bcc526b5aac06bb3d808e951222cda3a2fedd0
Rúbia Wardil <wardil.rubia@gmail.com>
2022-12-11 22:50:55 -0300

    dropdown menu in all pages

--- scripts/menu-navigation.js
+++ scripts/menu-navigation.js
@@ -14,20 +14,12 @@ function navigateImages(){
     window.location.href = '../pages/images-page.html';
 }
 
-function dropdownBtn_Click() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
+function dropdownBtn() {
+    let myDropdown = document.getElementById("myDropdown");
+    if(myDropdown.style.display=='block'){
+        myDropdown.style.display='none';
+    }
+    else{
+        myDropdown.style.display='block';
     }
   }
